1df9fc6272087d6f24827c1ff25f91a305988ffd
Swarupkumar Jana <swarup.jana@gmail.com>
2018-08-29 12:32:21 +0530

    Fresh commit for linkedlist data structure

---
+++ DataStructureExample/src/com/codetitans/datastructure/linkedlist/Node.java
@@ -0,0 +1,24 @@
+package com.codetitans.datastructure.linkedlist;
+
+public class Node<T> {
+
+	private T data;
+	private Node<T> next;
+
+	public Node(T data) {
+		this.data = data;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public Node<T> getNext() {
+		return next;
+	}
+
+}
